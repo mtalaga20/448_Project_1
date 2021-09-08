7bf5f90cfe6399736f9694ab2e1871813b51fbad
Adam Abernathy <amabernathy@ku.edu>
2021-09-08 11:14:09 -0500

    finish gameboard header, added to makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-battleship: main.o Executive.o
+battleship: main.o Executive.o gameBoard.o
 	g++ -g -std=c++11 -Wall main.o Executive.o -o battleship
 
-main.o: main.cpp Executive.hpp myDefines.h
+main.o: main.cpp Executive.hpp
 	g++ -g -std=c++11 -Wall -c main.cpp
 
-Executive.o: Executive.cpp Executive.hpp myDefines.h
+Executive.o: Executive.cpp Executive.hpp gameBoard.hpp
 	g++ -g -std=c++11 -Wall -c Executive.cpp
+
+gameBoard.o: gameBoard.cpp gameBoard.hpp
+	g++ -g -std=c++11 -Wall -c gameBoard.cpp
 
 clean:
 	rm *.o battleship
--- gameBoard.hpp
+++ gameBoard.hpp
@@ -12,6 +12,10 @@
 #ifndef GAMEBOARD_HPP
 #define GAMEBOARD_HPP
 
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <stdexcept>
 // #include "myDefines.h"
 
 using namespace std;
@@ -28,24 +32,68 @@ public:
     //default
     gameBoard();
 
-    //with par
-    //par: row - number of rows
-    //     col - number of columns
-    //     ships - number of ships in game
+    /** with param
+    @pre
+    @post
+    @param row - number of rows
+    @param col - number of columns
+    @param ships - number of ships in game */
     gameBoard(int row, int col, int ships);
 
     //destructor
     ~gameBoard();
 
     /*MARK: Setters*/
+    /** set number of rows
+    @pre
+    @post
+    @param rows - # of rows for map */
+    void set_rows(int row);
+
+    /** set number of columns
+    @pre
+    @post
+    @param col - # of columns */
+    void set_col(int col);
+
+    /** set number of ships
+    @pre
+    @post
+    @param ship - # of columns */
+    void set_ships(int ship);
 
     /*MARK: Getters*/
+    /** return num of rows
+    @pre
+    @post */
+    int get_rows() const;
 
+    /** return num of columns
+    @pre
+    @post */
+    int get_col() const;
+
+    /** return num of ships
+    @pre
+    @post */
+    int get_ships() const;
 
 
     /*MARK: Methods*/
-    //prints the game boar
+    /** prints the game boar
+    @pre
+    @post  */
     void printBoard();
+
+    /** prints the instructions
+    @pre
+    @post */
+    void printInstructions();
+
+    /** prints the legend
+    @pre
+    @post */
+    void printLegend();
 };
 
 #endif /* GAMEBOARD_HPP */
