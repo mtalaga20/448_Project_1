3f4694ca1942a0f1eaf0c2d65ed6ff14dc663680
Adam Abernathy <amabernathy@ku.edu>
2021-09-08 19:17:15 -0500

    added my defines header file

---
+++ myDefines.h
@@ -0,0 +1,35 @@
+//
+//  myDefines.h
+//  Abernathy_2876941_Lab_11
+//
+//  Created by Adam Abernathy on 11/16/2019.
+//  Copyright © 2019 Adam Abernathy. All rights reserved.
+//
+// file that include any #define and other constants
+// contains header linking
+//
+
+#ifndef MYDEFINES_H
+#define MYDEFINES_H
+
+//macros
+
+
+//C++ std libs
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <time.h>
+#include <random>
+
+//Class Header
+#include "Executive.hpp"
+#include "GameBoard.hpp"
+
+
+
+#endif /* MYDEFINES_H */
